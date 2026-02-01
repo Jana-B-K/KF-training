cd509bdf62705743355668d1b93f20f8e23e300e
Jana-B-K <janakumar9843@gmail.com>
2026-02-01 23:33:21 +0530

    create react app

---
+++ react-task/admin-dashboard/src/components/Form.jsx
@@ -0,0 +1,13 @@
+const Form = () => {
+    return (
+        <>
+            <form className="form" >
+                <input type="text" placeholder="Enter your email"/>
+                <input type="password" />
+                <button type="submit">Login in</button>
+            </form>
+        </>
+    )
+}
+
+export default Form;
