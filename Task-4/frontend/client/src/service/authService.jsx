b767dd9d1133eb80942b8592630449ea8ba8fb07
Jana <janakumar9843@gmail.com>
2026-02-03 18:47:40 +0530

    implement auth service

--- Task-4/frontend/client/src/service/authService.jsx
+++ Task-4/frontend/client/src/service/authService.jsx
@@ -2,17 +2,12 @@ import axios from 'axios';
 
 const API = "http://localhost:2000";
 
-export async function login(data){
-    const res = await axios.post(
-        `${API}/api/auth/login`,
-        data
-    )
-    return res.data;
+export async function login(data) {
+  const res = await axios.post(`${API}/api/auth/login`, data);
+  return res.data;
 }
 
-export async function register(data){
-    const res = await axios.post(
-        `${API}/api/auth/register`,
-        data
-    )
+export async function register(data) {
+  const res = await axios.post(`${API}/api/auth/register`, data);
+  return res.data; 
 }
