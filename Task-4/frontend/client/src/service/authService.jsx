c717a6f498093021913b06e30313c3ca8a96261c
Jana <janakumar9843@gmail.com>
2026-02-03 12:09:52 +0530

    implement auth logic for login and register

---
+++ Task-4/frontend/client/src/service/authService.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API = "http://localhost:2000";
+
+export async function login(data){
+    const res = await axios.post(
+        `${API}/api/auth/login`,
+        data
+    )
+    return res.data;
+}
+
+export async function register(data){
+    const res = await axios.post(
+        `${API}/api/auth/register`,
+        data
+    )
+}
