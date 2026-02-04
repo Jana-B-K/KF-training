efa5942c2a497161739f190f50036cb2b6326df1
Jana <janakumar9843@gmail.com>
2026-02-04 19:24:16 +0530

    Initialize Report and users

---
+++ react-task/admin-dashboard/src/components/Users.jsx
@@ -0,0 +1,11 @@
+import Layout from "./Layout";
+
+export default function Users() {
+  return (
+    <Layout>
+      <h2>User Management</h2>
+      <p>Manage your team members here.</p>
+      {/* Add your table or user list here */}
+    </Layout>
+  );
+}
