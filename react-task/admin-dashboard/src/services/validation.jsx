912d3bc7518d1fd1c9ac401e14c780f383cace44
Jana <janakumar9843@gmail.com>
2026-02-04 19:19:17 +0530

    Modify validation

--- react-task/admin-dashboard/src/services/validation.jsx
+++ react-task/admin-dashboard/src/services/validation.jsx
@@ -1,10 +1,10 @@
 export default function validateForm(data) {
-  const { username, password } = data;
+  const { email, password } = data;
   
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   
-  if (!emailRegex.test(username)) {
-    throw new Error("Username should be in email format");
+  if (!emailRegex.test(email)) {
+    throw new Error("email should be in email format");
   }
 
   if (password.length < 4 || password.length > 8) {
