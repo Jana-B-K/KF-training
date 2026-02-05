dcfeae41c243120ff05ae0a98b135e4c88a1a76d
Jana <janakumar9843@gmail.com>
2026-02-05 18:30:32 +0530

    Add new 10 extra datas

--- react-task/admin-dashboard/src/middleware/user.jsx
+++ react-task/admin-dashboard/src/middleware/user.jsx
@@ -1,32 +1,19 @@
 const users = [
-    {
-        id: 1,
-        name: "Jana",
-        email: "jana@gmail.com",
-        password: "1234",
-        role: "Admin",
-    },
-    {
-        id: 2,
-        name: "Nayeem",
-        email: "nayeem@gmail.com",
-        password: "1234",
-        role: "User",
-    },
-    {
-        id: 3,
-        name: "Nishan",
-        email: "nishan@gmail.com",
-        password: "1234",
-        role: "User",
-    },
-    {
-        id: 4,
-        name: "Girish",
-        email: "girish@gmail.com",
-        password: "1234",
-        role: "User",
-    },
-]
+    { id: 1, name: "Jana", email: "jana@gmail.com", password: "1234", role: "Admin", status: "Active" },
+    { id: 2, name: "Nayeem", email: "nayeem@gmail.com", password: "1234", role: "User", status: "Inactive" },
+    { id: 3, name: "Nishan", email: "nishan@gmail.com", password: "1234", role: "User", status: "Active" },
+    { id: 4, name: "Girish", email: "girish@gmail.com", password: "1234", role: "User", status: "Active" },
+    { id: 5, name: "Aarav", email: "aarav@gmail.com", password: "1234", role: "Admin", status: "Inactive" },
+    { id: 6, name: "Priya", email: "priya@gmail.com", password: "1234", role: "User", status: "Active" },
+    { id: 7, name: "Ishaan", email: "ishaan@gmail.com", password: "1234", role: "User", status: "Inactive" },
+    { id: 8, name: "Ananya", email: "ananya@gmail.com", password: "1234", role: "Admin", status: "Active" },
+    { id: 9, name: "Rahul", email: "rahul@gmail.com", password: "1234", role: "User", status: "Active" },
+    { id: 10, name: "Sanya", email: "sanya@gmail.com", password: "1234", role: "User", status: "Inactive" },
+    { id: 11, name: "Vikram", email: "vikram@gmail.com", password: "1234", role: "Admin", status: "Active" },
+    { id: 12, name: "Meera", email: "meera@gmail.com", password: "1234", role: "User", status: "Active" },
+    { id: 13, name: "Arjun", email: "arjun@gmail.com", password: "1234", role: "User", status: "Inactive" },
+    { id: 14, name: "Kavya", email: "kavya@gmail.com", password: "1234", role: "Admin", status: "Active" },
+    { id: 15, name: "Zayan", email: "zayan@gmail.com", password: "1234", role: "Admin", status: "Active" },
+];
 
 export default users;
